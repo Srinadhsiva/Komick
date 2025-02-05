496060f1d03734ddc6a455b18e6f3652cba1bb38
Srinadh <ranisrinadh@gmail.com>
2025-02-05 22:16:28 +0530

    8th commit

--- backend/index.js
+++ backend/index.js
@@ -4,15 +4,16 @@ const cors = require('cors')
 const router = require('./routes/router')
 const {cloudinaryConfig} = require('./config/cloudinaryConfig')
 
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
-app.use('*', cloudinaryConfig);
 app.use(cors({
     origin: 'https://komick-livid.vercel.app', // Allow only your frontend
     methods: 'GET,POST,PUT,DELETE',
     allowedHeaders: 'Content-Type,Authorization',
     credentials: true, // Allow cookies (if needed)
   }));
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use('*', cloudinaryConfig);
 app.use('/',router)
 
 app.listen(3000,(req,res)=>{
